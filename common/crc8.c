02f4230382c30a3d445a39b5706dcc4210c973a6
Paul Devine <pauldevine+git@gmail.com>
2024-05-24 16:05:07 +0200

    initial build version

--- common/crc8.c
+++ common/crc8.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stddef.h>
+#include "protocols.h"
 
 #define POLYNOMIAL 0x07
 
@@ -25,4 +26,32 @@ uint8_t crc8(const uint8_t *data, size_t len) {
         crc = crc8_table[crc ^ data[i]];
     }
     return crc;
+}
+
+uint8_t crc8_check(Command *cmd) {
+    uint8_t crc = 0x00;  // Initial value
+    crc = crc8_table[crc ^ cmd->protocol];
+    crc = crc8_table[crc ^ cmd->byte_count];
+    crc = crc8_table[crc ^ cmd->command];
+    for (size_t i = 0; i < cmd->byte_count; i++) {
+        crc = crc8_table[crc ^ cmd->params[i]];
+    }
+    if ( cmd->expected_crc == crc) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+uint8_t crc8_check(Data *data) {
+    uint8_t crc = 0x00;  // Initial value
+    crc = crc8_table[crc ^ cmd->byte_count];
+    for (size_t i = 0; i < cmd->byte_count; i++) {
+        crc = crc8_table[crc ^ cmd->buffer[i]];
+    }
+    if ( cmd->expected_crc == crc) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
