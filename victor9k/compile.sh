8530cf71bd43272456283db1f43462bd3b62e09e
Paul Devine <pauldevine+git@gmail.com>
2024-10-31 08:38:00 -0700

    added vcopy to compilation step

--- victor9k/compile.sh
+++ victor9k/compile.sh
@@ -3,3 +3,4 @@ cd /Users/pauldevine/projects/openwatcomv2/
 source ~/projects/openwatcomv2/mysetvars.sh
 cd /Users/pauldevine/projects/user_port_v9k/victor9k/src
 wmake clean all
+vcopy /Users/pauldevine/Desktop/randos/userport.img /Users/pauldevine/projects/user_port_v9k/victor9k/src/userport.sys :
