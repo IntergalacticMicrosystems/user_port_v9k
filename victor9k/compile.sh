1ed02bfade28b27e0f2b2a91c66623c73adcfac2
Paul Devine <spam@pdevine.com>
2024-12-14 07:24:42 -0800

    updated openwatcom compiler to latest version, some paths moved

--- victor9k/compile.sh
+++ victor9k/compile.sh
@@ -1,6 +1,6 @@
 #/bin/zsh
-cd /Users/pauldevine/projects/openwatcomv2/
-source ~/projects/openwatcomv2/mysetvars.sh
+cd /Users/pauldevine/projects/open-watcom-v2
+source ~/projects/open-watcom-v2/mysetvars.sh
 cd /Users/pauldevine/projects/user_port_v9k/victor9k/src
 wmake clean all
 vcopy /Users/pauldevine/Desktop/randos/userport.img /Users/pauldevine/projects/user_port_v9k/victor9k/src/userport.sys :
